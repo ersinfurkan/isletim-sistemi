c8ded1dc9b40a890bcb17dada996d7d0b46ebf7e
ersinfurkan <75623954+ersinfurkan@users.noreply.github.com>
2021-12-05 16:12:55 +0300

    Vize Ödevi
    Kasım Ayı İşletim Sistemi Vize Ödevi

---
+++ Vize_Ödevi İşletim Sİstemi.ps1
@@ -0,0 +1,33 @@
+﻿$Processes = @{}
+Get-Process -IncludeUserName | ForEach-Object {
+    $Processes[$_.Id] = $_
+}
+
+
+echo "TCP Daemons"
+Get-NetTCPConnection | 
+    Where-Object { $_.LocalAddress } |
+    Select-Object LocalAddress,
+        LocalPort,
+
+        @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }} |
+    Sort-Object -Property LocalPort, UserName |
+    Format-Table -AutoSize |  Out-File -FilePath C:\Users\FURKAN\Desktop\TCP.txt
+
+    Get-Content -Path C:\Users\FURKAN\Desktop\TCP.txt
+
+
+
+echo "UDP Daemons"
+Get-NetUDPEndpoint | 
+    Where-Object { $_.LocalAddress} |
+    Select-Object LocalAddress,
+        LocalPort,
+       
+        @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }} |
+    Sort-Object -Property LocalPort, UserName |
+    Format-Table -AutoSize |  Out-File -FilePath C:\Users\FURKAN\Desktop\UDP.txt 
+
+
+Get-Content  -Path  C:\Users\FURKAN\Desktop\UDP.txt 
+
